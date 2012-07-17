e6c3bfca4631bf3e18b9db78c121a607e90a2004
Dustin Smith <dustin@media.mit.edu>
2012-07-17 12:01:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Gitpass
+
+An extension of the Python standard library's [getpass](http://docs.python.org/library/getpass.html) module designed for
+(not) committing a password to a Git repository
+
+## Usage
+
+    import gitpass
+
+    aws_pwd = gitpass.gitpass('AWS Password')
+
+The first time you use this in your git repository, it will:
+
+  1. Prompt you for a password in the terminal
+  2. Create a file called `.__aws_password` that contains your password
+     in plaintext
+  3. Add `.__aws_password` to your `.gitignore` file.  
+
+The next time you use the password, it will not prompt you.
+
---
+++ gitpass.py
@@ -0,0 +1,97 @@
+"""
+An extension of the Python standard library's 'getpass' module designed for
+(not) committing a password to a Git repository
+
+Copyright (C) 2012 Dustin Smith
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+import os
+import re
+import logging
+import getpass
+
+def find_git_directory(prefix=None):
+    """
+    Traverses the directory starting from 'prefix' (or this file's path)
+    for .git and returns that directory or False if none can be found
+    """
+    if prefix is None:
+        prefix = os.path.dirname(os.path.abspath(__file__))
+    if os.path.isdir(prefix+'/.git'):
+        return prefix
+    else:
+        prevfix = prefix
+        prefix = os.path.abspath(prefix + "/" + os.path.pardir)
+        if prefix == prevfix:
+            return False
+        else:
+            return find_git_directory(prefix)
+
+def find_in_gitignore(gitdir, entry):
+    with open(gitdir + "/.gitignore") as f:
+        for line in f:
+            if line.strip() == entry:
+                return True
+    return False
+
+
+def ensure_in_gitignore(gitdir, entry):
+    try:
+        if find_in_gitignore(gitdir, entry):
+            return True
+    except IOError:
+        # .gitignore doesn't exit
+        logging.info("Creating %s/.gitignore" % (gitdir,))
+    logging.info("Adding %s to .gitignore" % (entry,))
+    with open(gitdir + "/.gitignore", 'a') as f:
+        f.write("%s\n" % (entry,))
+        f.close()
+
+def save_password(passfile, password):
+    with open(passfile, 'w') as f:
+        f.write("%s\n" % (password,))
+        f.close()
+
+def gitpass(prompt, passfile=None, force_new=False):
+    """
+    This prompts the user with 'prompt' in the terminal and then
+    creates a file (named 'passfile' if specified, otherwise based on
+    the prompt) to store the password in for the future.
+
+    force_new asks the user for a prompt at each time.
+    """
+   
+    passfile = passfile or re.sub(r'\s', '_', prompt.lower().strip())
+    hidden_passfile = ".__%s" % passfile
+    gitdir = find_git_directory()
+    passpath = gitdir + "/" + hidden_passfile
+    try:
+        if force_new:
+            raise IOError 
+        with open(passpath) as pf:
+            password = pf.read().strip()
+            print "Read password ", password
+        return password
+    except IOError:
+        password = raw_input(prompt + ":").strip()
+        ensure_in_gitignore(gitdir, hidden_passfile)
+        save_password(passpath, password)
+        return password
+
