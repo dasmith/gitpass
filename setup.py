f3eaea9b4cd9fd2d5de75e1a26334298b5243827
Dustin Smith <dustin@media.mit.edu>
2012-08-02 13:34:32 -0400

    separated description onto two lines

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 
 setup(name='gitpass',
       version='0.1',
-      description="Gitpass is an extension of the Python standard library's
-      getpass, designed for keeping passwords out of your git repository.",
+      description="Gitpass is an extension of the Python standard library's " \
+      + "getpass, designed for keeping passwords out of your git repository.",
       author='Dustin Smith',
       author_email='dustin@media.mit.edu',
       download_url='https://github.com/dasmith/gitpass/tarball/master',
